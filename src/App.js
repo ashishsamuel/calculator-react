8c96307b5ce423b6af24e8255ab4333e37522155
ashish <ashishsamuelthomson@gmail.com>
2023-11-01 09:43:21 +0530

    calculator app completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { Button } from '@mui/material';
 import './App.css';
 import { useState } from 'react';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 function App() {
   const [result, setResult] = useState('');
@@ -51,51 +49,39 @@ function App() {
         </div>
         <div className='bg-info border rounded mt-2 p-3'>
             <div className='bg-dark p-3'>
-                <span className='p-2'>{input}</span>
-                {result !='' && <span>{result}</span>
-                }
+              <span className='p-2'>{input}</span>
+              {result !='' && <span>{result}</span>
+              }
             </div>
-            <Row className='mt-2'>
-            <Col>
-              <Button className='m-1 bg-danger btn-hoverstyle' variant="contained" onClick={() => getValue('C')} >C</Button>
-              <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('DEL')} >DEL</Button>
-              <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('.')} >.</Button>
-              <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('/')} >/</Button>
-            </Col>
-            </Row>
-            <Row> 
-           <Col>
-              <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('7')} >7</Button>
-              <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('8')} >8</Button>
-              <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('9')} >9</Button>
-              <Button className='m-1 bg-dark btn-hoverstyle'variant="contained" onClick={() => getValue('*')} >*</Button>
-           </Col>
-            </Row>
-            <Row>
-            <Col>
-              <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('4')} >4</Button>
-              <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('5')} >5</Button>
-              <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('6')} >6</Button>
-              <Button className='m-1 bg-dark btn-hoverstyle'variant="contained" onClick={() => getValue('-')} >-</Button>
-            </Col>
-            </Row>
-            <Row>
-            <Col>
-              <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('1')} >1</Button>
-              <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('2')} >2</Button>
-              <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('3')} >3</Button>
-              <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('+')} >+</Button>
-            </Col>
-            </Row>
-            <Row>
-           
-              <Col>
-                <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('00')} >00</Button>
-                <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('0')} >0</Button>
-                <Button className='m-1 bg-warning btn-hoverstyle' style={{width:'140px'}} variant="contained" onClick={() => getValue('=')} >=</Button>
-              </Col>
-              
-           </Row>
+            <div className='mt-2'>
+            <Button className='m-1 bg-danger btn-hoverstyle' variant="contained" onClick={() => getValue('C')} >C</Button>
+            <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('DEL')} >DEL</Button>
+            <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('.')} >.</Button>
+            <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('/')} >/</Button>
+            </div>
+            <div> 
+            <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('7')} >7</Button>
+            <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('8')} >8</Button>
+            <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('9')} >9</Button>
+            <Button className='m-1 bg-dark btn-hoverstyle'variant="contained" onClick={() => getValue('*')} >*</Button>
+            </div>
+            <div>
+            <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('4')} >4</Button>
+            <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('5')} >5</Button>
+            <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('6')} >6</Button>
+            <Button className='m-1 bg-dark btn-hoverstyle'variant="contained" onClick={() => getValue('-')} >-</Button>
+            </div>
+            <div>
+            <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('1')} >1</Button>
+            <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('2')} >2</Button>
+            <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('3')} >3</Button>
+            <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('+')} >+</Button>
+            </div>
+            <div>
+            <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('00')} >00</Button>
+            <Button className='m-1 bg-dark btn-hoverstyle' variant="contained" onClick={() => getValue('0')} >0</Button>
+            <Button className='m-1 bg-warning btn-hoverstyle' style={{width:'140px'}} variant="contained" onClick={() => getValue('=')} >=</Button>
+            </div>
           </div>
       </div>
      
